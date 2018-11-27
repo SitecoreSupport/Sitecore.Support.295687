aeeaf51f970089d93624282be9fc29fefc7763b1
Aleksandr Chykun <AEC@sitecore.net>
2018-11-27 11:04:55 +0200

    commit patch

---
+++ src/Sitecore.Support.295687/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.295687")]
+[assembly: AssemblyProduct("Sitecore.Support.295687")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.295687/ResizeProcessor.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Resources.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Resources.Media
+{
+  public class ResizeProcessor: Sitecore.Resources.Media.ResizeProcessor
+  {
+    public void Process(GetMediaStreamPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (!string.Equals(args.MediaData.MimeType, "image/gif", StringComparison.OrdinalIgnoreCase))
+      {
+        base.Process(args);
+      }
+    }
+  }
+}
