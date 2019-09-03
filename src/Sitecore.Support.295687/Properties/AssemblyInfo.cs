1d244496fe30be22cd735c6bb19be75d6be5c469
Paul Stupka <paul.stupka@sitecore.com>
2019-09-03 13:44:46 +0300

    add assembly description

--- src/Sitecore.Support.295687/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.295687/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.295687")]
 [assembly: AssemblyProduct("Sitecore.Support.295687")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("skips resizing for gif images")]
+
