36b0282b7ccd5504516a808759cd4449da54aeda
Paul Stupka <paul.stupka@sitecore.com>
2019-09-03 13:38:43 +0300

    avoid obsolete constructor usage, fix files structure, remove unsued namespaces, show that method hiding is intentional

---
+++ src/Sitecore.Support.295687/Resources/Media/ResizeProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Abstractions;
+using Sitecore.DependencyInjection;
+using Sitecore.Diagnostics;
+using Sitecore.Resources.Media;
+
+namespace Sitecore.Support.Resources.Media
+{
+  public class ResizeProcessor: Sitecore.Resources.Media.ResizeProcessor
+  {
+    public ResizeProcessor() : base(ServiceLocator.ServiceProvider.GetRequiredService<BaseMediaManager>(),
+      ServiceLocator.ServiceProvider.GetRequiredService<BaseLog>())
+    {
+    }
+
+    public new void Process(GetMediaStreamPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (!string.Equals(args.MediaData.MimeType, "image/gif", StringComparison.OrdinalIgnoreCase))
+      {
+        base.Process(args);
+      }
+    }
+  }
+}
